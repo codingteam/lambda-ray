a1e2050f7f3f8c2fe0ebb30d07bf1dd9acc18b12
Alex Newlifer <alex.newlifer@gmail.com>
2015-02-12 14:39:16 +0300

    File rename

---
+++ src/Interfaces/IRenderConsumer.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+namespace core {
+
+/**
+ * @brief Interface for handling render output.
+ */
+class IRenderConsumer {
+public:
+    IRenderConsumer() {}
+
+    virtual ~IRenderConsumer() {}
+
+    /**
+     * @brief Something for handling...
+     */
+    //virtual void handler( ... ) = 0;
+
+};
+
+} // namespace core
